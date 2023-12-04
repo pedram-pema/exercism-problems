a2b49a01b9c1e6e286325248c5f17e476fdbee51
pedram-pema <studentpedramp@gmail.com>
2023-12-04 18:57:33 +0330

    Complete 3 problems of the exercise

--- javascript/freelancer-rates/freelancer-rates.js
+++ javascript/freelancer-rates/freelancer-rates.js
@@ -26,7 +26,7 @@
  * @returns {number} the rate per day
  */
 export function dayRate(ratePerHour) {
-  throw new Error('Remove this line and implement the function');
+  return ratePerHour * 8;
 }
 
 /**
@@ -37,7 +37,7 @@ export function dayRate(ratePerHour) {
  * @returns {number} the number of days
  */
 export function daysInBudget(budget, ratePerHour) {
-  throw new Error('Remove this line and implement the function');
+  return Math.floor(budget / (ratePerHour * 8));
 }
 
 /**
@@ -49,5 +49,9 @@ export function daysInBudget(budget, ratePerHour) {
  * @returns {number} the rounded up discounted rate
  */
 export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
-  throw new Error('Remove this line and implement the function');
+  let fullMonths = Math.floor(numDays / 22);
+  let fullMonthPrice = ratePerHour * 8 * 22 * fullMonths;
+  let discountedPrice = fullMonthPrice - (fullMonthPrice * discount);
+  let dayRatePrice = (numDays % 22) * 8 * ratePerHour;
+  return Math.ceil(discountedPrice + dayRatePrice);
 }
